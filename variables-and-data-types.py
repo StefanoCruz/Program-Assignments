b5aa10b0d6345e4afecc800b61dfea8ff9e7e5a1
StefanoCruz <92397475+StefanoCruz@users.noreply.github.com>
2021-10-28 12:33:02 -0500

    Test save python

---
+++ variables-and-data-types.py
@@ -0,0 +1,20 @@
+print("Hello World")
+my_text="Hello World"
+print(my_text)
+print(1, 2, 4 , 5,)
+glass_of_water= 3
+print("I drank", glass_of_water, "glasses of water today.")
+glass_of_water=3
+glass_of_water=glass_of_water + 1
+print(glass_of_water)
+men_stepped_on_the_moon=1
+print(men_stepped_on_the_moon)
+my_reason_for_coding="I want to see if I like coding, it is a skill that I have never attempted"
+print(my_reason_for_coding)
+global_mean_sea_level_2018=21
+global_mean_sea_level_2018=21.21
+global_mean_sea_level_2018=21.21
+global_mean_sea_level_2018=21.21
+print(global_mean_sea_level_2018)
+staying_alive= True
+print(staying_alive)
