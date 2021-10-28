0d414d2ddfd61ff713e0588d88b6d6947ea30ed0
StefanoCruz <92397475+StefanoCruz@users.noreply.github.com>
2021-10-28 13:27:39 -0500

    finished python variables

--- variables-and-data-types.py
+++ variables-and-data-types.py
@@ -18,3 +18,46 @@ global_mean_sea_level_2018=21.21
 print(global_mean_sea_level_2018)
 staying_alive= True
 print(staying_alive)
+men_stepped_on_the_moon=12
+answer_1= type(men_stepped_on_the_moon)
+print(answer_1)
+my_reason_for_coding= "intergalactic travel"
+answer_2= type(my_reason_for_coding)
+print(answer_2)
+global_mean_sea_level_2018= 21.21
+answer_3= type(global_mean_sea_level_2018)
+print(answer_3)
+staying_alive= True
+answer_4= type(staying_alive)
+print(answer_4)
+my_grade=10
+answer_5= type(my_grade)
+print(answer_5)
+my_temp = 97.70
+answer_6= int(my_temp)
+print(type(answer_6))
+shoe_price = "69.99"
+answer_7= float(shoe_price)
+print(type(answer_7))
+gross_world_product=84.84
+gwp_str= str(gross_world_product)
+answer_8="In 2018 gross product of the world (GWP) was " + gwp_str + " in trillion US dollars."
+print(answer_8)
+gift_list = []
+answer_9= type(gift_list)
+print(answer_9)
+grocery_items= {}
+answer_10= type(grocery_items)
+print(answer_10)
+bucket_list= ()
+answer_11=type(bucket_list)
+print(answer_11)
+gift_list= [1, 2 ,3 ,4]
+print(gift_list)
+grocery_items= {"apples": 1,
+ "oranges": 2,
+  "bananas": 5
+  }
+print(grocery_items)
+bucket_list= ("Go to a concert", "Visit Japan", "Skydive")
+print(bucket_list)
