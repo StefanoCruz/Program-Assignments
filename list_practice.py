ae3ad8a2793e90c3bfadf4180b526b84f6adced0
StefanoCruz <92397475+StefanoCruz@users.noreply.github.com>
2021-11-03 12:16:07 -0500

    final save list_practice.py

---
+++ list_practice.py
@@ -0,0 +1,41 @@
+Cities = ['Oakland', 'Atlanta', 'New York City', 'Seattle', 'Memphis', 'Miami', 'Boston', 'Los Angeles', 'Denver', 'New Orleans']
+print(Cities)
+Assassins_Creed_Games = ['1.Assassin’s Creed (2007)',
+'2. Assassin’s Creed II (2009)',
+'3. Assassin’s Creed: Brotherhood (2010)',
+'4. Assassin’s Creed: Revelations (2011)',
+'5. Assassin’s Creed III (2012)',
+'6. Assassin’s Creed IV: Black Flag (2013)',
+'7. Assassin’s Creed: Rogue (2014)',
+'8. Assassin’s Creed: Unity (2014)',
+'9. Assassin’s Creed: Syndicate (2015',
+'10. Assassin’s Creed: Origins (2017)',
+'11. Assassin’s Creed: Odyssey (2018)',
+'12. Assassin’s Creed: Valhalla (2021)']
+print(Assassins_Creed_Games)
+print(Cities[2])
+print(Cities[-1])
+print(Cities[-2])
+print(Assassins_Creed_Games[0])
+print(Assassins_Creed_Games[-3])
+print(Assassins_Creed_Games[-5])
+print(Cities[0:4])
+print(Assassins_Creed_Games[4:8])
+Cities[0] = 'San Francisco' 
+print(Cities)
+Cities[2] = 'Brooklyn'
+Cities[7] = 'Hollywood'
+Cities[5] = 'Tampa'
+print(Cities)
+Cities . append ('Oakland')
+print(Cities)
+Cities . extend (['Los Angeles', 'New York City,'])
+print(Cities)
+Cities . insert (0, 'Miami')
+print(Cities)
+del Cities [3]
+print(Cities)
+Cities.pop (6)
+print(Cities)
+Cities.remove('Atlanta')
+print(Cities)
