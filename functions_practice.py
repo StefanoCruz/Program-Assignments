57b3a3a7a181d4808ac66f224d25e3cf9387c3b7
StefanoCruz <92397475+StefanoCruz@users.noreply.github.com>
2021-11-08 12:28:05 -0600

    save functions practice

--- functions_practice.py
+++ functions_practice.py
@@ -4,80 +4,91 @@ food_prices = {
 "Cheese:$1.00",
 "Milk:$2.50"
 }
-print(food_prices)
 Wake_Up_Time = {
 'Monday' : '7:00am',
 'Tuesday' : '7:06am',
 'Wednesday' : '8:00am',
 'Thursday' : '7:30am'}
-print(Wake_Up_Time)
 food_prices = {
-'Chicken': '$1.59', 
-'Beef': '$1.99',
-'Cheese': '$1.00',
-'Milk': '$2.50'
+'Chicken': 1.59, 
+'Beef': 1.99,
+'Cheese': 1.00,
+'Milk': 2.50
 }
 chicken_price = food_prices['Chicken']
-print(chicken_price)
 beef_price = food_prices['Beef']
 cheese_prices = food_prices['Cheese']
 milk_prices = food_prices['Milk']
-print(beef_price)
-print(cheese_prices)
-print(milk_prices)
 monday_wake_up_time = Wake_Up_Time['Monday']
-print(monday_wake_up_time)
 tuesday_wake_up_time = Wake_Up_Time['Tuesday']
 wednesday_wake_up_time = Wake_Up_Time['Wednesday']
 thurday_day_wake_up_time = Wake_Up_Time['Thursday']
-print(tuesday_wake_up_time)
-print(wednesday_wake_up_time)
-print(thurday_day_wake_up_time)
 shoe_inventory = { 'Jordan' : 1,
 'Yeezy' :8,
 'Foamposite' : 10,
 'Air Max' : 5,
 'SB Dunk' : 20
 }
-print(shoe_inventory)
 shoe_inventory['SB Dunk'] -= 2
 shoe_inventory['Yeezy']+=1
-print(shoe_inventory)
 shoe_inventory['Jordan']+= 7
 shoe_inventory['Yeezy']+= 7
 shoe_inventory['Foamposite']+= 7
 shoe_inventory['Air Max']+= 7
 shoe_inventory['SB Dunk']+= 7
-print(shoe_inventory)
 shoe_inventory['Jordan'] -= 3
 shoe_inventory['Yeezy'] -= 3
 shoe_inventory['Foamposite'] -= 3
 shoe_inventory['Air Max'] -= 3
 shoe_inventory['SB Dunk'] -= 3
-print(shoe_inventory)
 shoe_inventory['Vans']=8
 shoe_inventory['Converse']=7
 shoe_inventory['Adidas']=6
-print(shoe_inventory)
 Wake_Up_Time['Friday']= '10:00am' 
 Wake_Up_Time['Saturday']= '11:00am'
 Wake_Up_Time['Sunday']= '12:00pm'
-print(Wake_Up_Time)
-food_prices['Eggs'] = '$1.20'
-food_prices['Bacon'] = '$4.00'
-food_prices['Cereal'] = '$2.00' 
-print(food_prices)
+food_prices['Eggs'] = 1.20
+food_prices['Bacon'] = 4.00
+food_prices['Cereal'] = 2.00 
 del shoe_inventory['Converse']
 del shoe_inventory['Adidas']
 del Wake_Up_Time['Friday']
 del Wake_Up_Time['Saturday']
 del food_prices['Beef']
 del food_prices['Bacon']
-print(shoe_inventory)
-print(Wake_Up_Time)
-print(food_prices)
 #arithmetic operators
-def total_price(Chicken, Cheese) :
-    Price = food_prices[Chicken]+food_prices[Cheese]
-    return Price
-    print(Price)
+def total_price(food_item1, food_item2):
+    total_price = food_prices[food_item1]+food_prices[food_item2]
+    return total_price
+
+print(total_price('Cheese','Cereal')) 
+
+def price_difference(food_item1, food_item2):
+    price_difference= food_prices[food_item1]-food_prices[food_item2]
+    return abs(price_difference)
+
+print(price_difference('Cheese','Cereal'))
+
+def shoe_restock(shoe,num):
+    shoe_inventory[shoe] *= num
+    return shoe_inventory
+
+print(shoe_restock('Yeezy',3))
+
+def clearance_sale(shoe,num):
+    shoe_inventory[shoe] //= num
+    return shoe_inventory
+
+print(clearance_sale('SB Dunk', 2))
+
+def restock_alert(dict):
+    smallest=5
+    supply= ''
+
+    for key in dict.keys():
+        if dict[key]<=smallest:
+            smallest=dict[key]
+            supply = key
+    return (supply,smallest)
+
+print(restock_alert(shoe_inventory))
